370ef5261e392a8dc05b813c41adc75a028dd9ca
maksimmanzhai <bender23max@gmail.com>
2019-09-12 19:51:27 +0300

    Program about area of triangle is well done

--- lesson1_2.rb
+++ lesson1_2.rb
@@ -1,8 +1,6 @@
 puts "Enter size of base"
 a = gets.chomp.to_f
-puts a
 puts "Enter size of height"
 height = gets.chomp.to_f
-puts height
 area_of_triangle = ( 1.0 / 2.0 ) * a * height
 puts "Area of a triangle is #{area_of_triangle}"
